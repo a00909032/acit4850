0792026da41355b2b87b5487cc8097bdfb61d483
dkim <dkim@Damian>
2016-01-07 12:54:01 -0800

    lab1 part3

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 $name = 'Jim';
 $what = 'geek';
 $level = 10;
-echo 'Hi, my name is '.$name,'. and I am a level '.$level.' '.$what;
+echo 'Hi, my name is '.$name.', and I am a level '.$level.' '.$what;
+echo '<br/>';
+$hoursworked = 10;
+$rate = 12;
+$total = $hoursworked * $rate;
+echo 'You owe me '.$total;
 
 ?>
