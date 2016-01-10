d5dd2702c3442b26e269bceafa1a29a2d32afed9
dkim <dkim@Damian>
2016-01-10 07:25:18 -0800

    lab1 tictactoe part3

--- index.php
+++ index.php
@@ -1,69 +1,45 @@
 <?php
 
 $position = $_GET['board'];
-$squares = str_split($position);
+$squares = $position;
 
-function winner($token, $position) {
-    $won = false;
-    for ($row = 0; $row < 3; $row++) { //check for vertical and horizontal 
-        if (($position[3 * $row] == $token) && ($position[3 * $row + 1] == $token) && ($position[3 * $row + 2] == $token))
-            $won = true;
-        else if (($position[$row] == $token) && ($position[$row + 3] == $token) && ($position[$row + 6] == $token))
-            $won = true;
-    }
-    //check for 2 diagonal 
-    if (($position[0] == $token) &&
-            ($position[4] == $token) &&
-            ($position[8] == $token)) {
-        $won = true;
-    } else if (($position[2] == $token) &&
-            ($position[4] == $token) &&
-            ($position[6] == $token)) {
-        $won = true;
+class Game {
+
+    var $position;
+
+    function __construct($squares) {
+        $this->position = str_split($squares);
     }
 
-    /*
-      $won = false;
-      if (($position[0] == $token) &&
-      ($position[1] == $token) &&
-      ($position[2] == $token)) {
-      $won = true;
-      } else if (($position[3] == $token) &&
-      ($position[4] == $token) &&
-      ($position[5] == $token)) {
-      $won = true;
-      } else if (($position[6] == $token) &&
-      ($position[7] == $token) &&
-      ($position[8] == $token)) {
-      $won = true;
-      } else if (($position[0] == $token) &&
-      ($position[3] == $token) &&
-      ($position[6] == $token)) {
-      $won = true;
-      } else if (($position[1] == $token) &&
-      ($position[4] == $token) &&
-      ($position[7] == $token)) {
-      $won = true;
-      } else if (($position[2] == $token) &&
-      ($position[5] == $token) &&
-      ($position[8] == $token)) {
-      $won = true;
-      } else if (($position[0] == $token) &&
-      ($position[4] == $token) &&
-      ($position[8] == $token)) {
-      $won = true;
-      } else if (($position[2] == $token) &&
-      ($position[4] == $token) &&
-      ($position[6] == $token)) {
-      $won = true;
-      } */
-    return $won;
+    function winner($token) {
+        $won = false;
+        for ($row = 0; $row < 3; $row++) { //check for vertical and horizontal 
+            if (($this->position[3 * $row] == $token) && ($this->position[3 * $row + 1] == $token) && ($this->position[3 * $row + 2] == $token)) {
+                $won = true;
+            } else if (($this->position[$row] == $token) && ($this->position[$row + 3] == $token) && ($this->position[$row + 6] == $token)) {
+                $won = true;
+            }
+        }
+        //check for 2 diagonal 
+        if (($this->position[0] == $token) &&
+                ($this->position[4] == $token) &&
+                ($this->position[8] == $token)) {
+            $won = true;
+        } else if (($this->position[2] == $token) &&
+                ($this->position[4] == $token) &&
+                ($this->position[6] == $token)) {
+            $won = true;
+        }
+        return $won;
+    }
+
 }
 
-if (winner('x', $squares))
-    echo 'X win.';
-else if (winner('o', $squares))
-    echo 'O win.';
+$game = new Game($squares);
+if ($game->winner('x'))
+    echo 'X wins';
+else if ($game->winner('o'))
+    echo 'O wins';
 else
-    echo 'No winner yet.';
+    echo 'No winner yet';
 ?>
